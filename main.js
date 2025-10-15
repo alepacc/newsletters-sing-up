b776d12579f6a9d4e91756738b60c201f24d18a1
alepacc <alessiopacconi@gmail.com>
2025-10-15 16:11:48 +0200

    first commit

--- main.js
+++ main.js
@@ -7,13 +7,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Initialization code here
     console.log('Newsletter Sign-Up script loaded.');
+
 });
 
+const card = document.querySelector(".card");
 const divSucces = document.querySelector('.card__success');
 const form = document.querySelector('form');
+const btnDismiss = document.querySelector('.cardsuccess_btn');
 
 form.onsubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted');
+    // console.log('Form submitted');
     divSucces.style.display = "flex";
+    card.style.display = "none";
 }
+
+
+ if (btnDismiss) {
+    btnDismiss.addEventListener('click', (e) => {
+        e.preventDefault();
+        // console.log('Dismiss pressed');
+        if (divSucces) divSucces.style.display = 'none', card.style.display = "flex";
+        
+    });
+} else {
+    console.warn('Dismiss button not found: .cardsuccess_btn');
+}
+
+
